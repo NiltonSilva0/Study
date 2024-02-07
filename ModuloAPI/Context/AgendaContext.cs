11866d7581df4a32fe97b8036fe8974348c8fe3e
NiltonSilva0 <niltonsilva.code@outlook.com>
2024-02-07 12:30:45 -0300

    API

---
+++ ModuloAPI/Context/AgendaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModuloAPI.Entities;
+
+namespace ModuloAPI.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options) //CONSTRUCTOR recebendo um options e passando para DbContext
+        {
+        }
+
+        public DbSet<Contato> Contatos { get; set; }//VINCULA a entidade Contatos no DbSet do tipo Contato, identificando como tabela
+    }
+}
---
+++ ModuloAPI/Controllers/ContatoController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModuloAPI.Context;
+using ModuloAPI.Entities;
+
+namespace ModuloAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase //CONTROLLER
+    {
+        private readonly AgendaContext _context;
+        public ContatoController(AgendaContext context) //CONSTRUCT
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contato contato) //ENDPOINT CREATE
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(ObterPorId), new {id = contato.Id}, contato);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id) //ENDPOINT OBTER POR ID
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return NotFound();
+            return Ok(contato);
+        }
+
+        [HttpGet("ObterPorNome")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            var contatos = _context.Contatos.Where(x => x.Nome.Contains(nome));
+            return Ok(contatos);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contato) //ENDPOINT UPDATE 
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if (contatoBanco == null)
+                return NotFound();
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if (contatoBanco == null)
+                return NotFound();
+
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+        
+    }
+}
