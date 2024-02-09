b495e99d9608f1f5972f6650c609b0fdeff21fcf
NiltonSilva0 <niltonsilva.code@outlook.com>
2024-02-09 15:50:43 -0300

    mvc

---
+++ PROJETOMVC/Context/AgendaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PROJETOMVC.Models;
+
+namespace PROJETOMVC.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Contato> Contatos { get; set; }
+        
+    }
+
+
+}
---
+++ PROJETOMVC/Controllers/ContatoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROJETOMVC.Context;
+using PROJETOMVC.Models;
+
+namespace PROJETOMVC.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)//INJEÇÃO DE DEPENDENCIA para o _context
+        {
+            _context = context;
+        }
+        public IActionResult Index() 
+        {
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+    }
+}
