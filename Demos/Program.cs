d37f47270e0cd11e05f4c78ad1ee1efcbbd01dea
Nilton Silva <niltonsilva.code@outlook.com>
2024-02-22 09:25:05 -0300

    #

---
+++ Demos/Program.cs
@@ -0,0 +1,27 @@
+﻿//int[] numeros = new int[] {0,2,4,6,8,10};
+//Console.Write("Digite o número que quer encontrar: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine();
+
+
+//static int EncontrarNumero(int[] numeros, int numero)
+//{
+//    for (int i = 0; i < numeros.Length; i++)
+//    {
+//        if (numeros[i] == numero) 
+//         return i; 
+//    }
+//    return -1;
+//}
+
+//var numEncontrado = EncontrarNumero(numeros, num);
+
+//if (numEncontrado >= 0)
+//{
+//    Console.WriteLine($"O número digitado está na posição: {numEncontrado}");
+//}
+//else
+//{
+//Console.WriteLine("O número não foi encontrado!");
+//}
+
