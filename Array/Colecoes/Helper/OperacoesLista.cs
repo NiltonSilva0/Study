4e2d9c2e00d2827e8773a663f44b6320db6246e1
Nilton Silva <niltonsilva.code@gmail.com>
2024-10-31 11:50:30 -0300

    Listas e arrays

---
+++ Array/Colecoes/Helper/OperacoesLista.cs
@@ -0,0 +1,16 @@
+﻿namespace Colecoes.Helper;
+
+public class OperacoesLista
+{
+    public void ImprimirLista(List<string> lista)
+    {
+        foreach (var item in lista)
+        {
+            Console.WriteLine($"Estados na lista original: {item}");
+        }
+    }
+    public void RemoverLista(List<string> lista, string estado)
+    {
+        lista.Remove(estado);
+    }
+}
